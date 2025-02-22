4b5e1b0f9f9431f27f4ccf846b58a56d38c47c37
ninoo88 <https://github.com/ninoo88/GOA-homeworks.git>
2025-02-22 19:17:39 +0400

    commit

--- Day01/Homework/hw.py
+++ Day01/Homework/hw.py
@@ -1,5 +1,51 @@
-name = "Nino"
-surname = "Talakhadze"
-age = "11"
-hobby ="drawing"
-print(name, surname, age, hobby)
+i = "i"
+am = "am"
+a = "and"
+dot = "."
+um = "i'm"
+y = "years"
+ol = "old"
+age = "eleven"
+height = "one meter and seventy centimeters"
+lo = "love"
+trav = "travelling"
+exp = "experiencing"
+ne = "new"
+th = "things"
+
+
+name = "nino"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნოშვნელობის მომნიჭებელი სიმბოლო
+# "nino" არის ცვლადისთვის მნიშვნელობა
+
+surname = "talakhadze"
+
+# print (name) 
+# პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+# name = "nino"   ეს არის str (string)  ტიპის ცვლადი
+# age = 11 ეს არის int ( integer ) მთელი რიცხვი
+# height = 170,5 ეს არის float ტიპის ცვლადი ( ათწილადი )
+#boolean (bool) ტიპის ცვლადი
+
+knows_programming = True  #true ან false
+is_ugly = False #snakecase ( უბრალოდ წერის სტილი სტანდარტულად)
+
+isUgly = False # ჯავასკრიპტული camelcase
+
+# print (name + " " + surname)
+
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოაბი
+# print (name + age)
+
+
+print(type(age))
+print(type(name))
+print(type(surname))
+print(type(height))
+print(type(knows_programming))
+
+print (name + " " + str(age))
+
+print (i + " " + am + " " + name + " " + surname  + " " + a + " " + um + " " + age + " " + y + " " + ol + dot + " " + i + " " + am + " " + height + dot + " " + i + " " + lo + " " + trav + " " + a + " " + exp + " " + ne + " " + th + dot )
--- Day04/Homework/hw.py
+++ Day04/Homework/hw.py
@@ -1,1 +1,48 @@
-#in progress
+fruit = "strawberry"
+furit = "orange"
+fruit = "dragon fruit"
+fruit = "lemon"
+fruit = "watermelon"
+
+name = "Ekuna"
+name = "Ana"
+name = "Tamuna"
+name = "Dato"
+name = "Nino"
+
+pet = "cat"
+pet = "fish"
+pet = "rabit"
+pet = "parrot"
+pet = "chihuahua"
+
+print(fruit)
+print(name)
+print(pet)
+
+
+
+# 1
+num1 = 'one'  # აქ ეწერა num1 = ;one;   one უნდა იწერeბოდეს ბრჭყალებში.
+print(num1)
+# 2
+num2 = 'two'  #აქ ეწერა  num2 = ('two')  two უნდა იწერებოდეს მხოლოდ ბრჭყალებში და არა ფრჩხილებში. ასევე მამგონი print-ი აკლდა
+print(num2)
+# 3
+Num3 = 'three'  # აქ ეწერა Num3 = 'three' და print-ში ეწერა print(num3). თუ დააკვირდები print-ში დიდი ასოთი არ წერია Num3
+print(Num3)
+# 4
+num4 = 'number4'  # აქ ეწერა num4 = 'number4" იგივე ნაირ ფრჩხილში უნდა იყოს მოთავსებული number4
+print(num4)
+# 5
+num5 = 'five'  # აქ ეწერა 5num = 'five' print(5num). ჯერ უნდა იყოს მოთავსებული სიტყვა num და შემდაგ 5
+print(num5)
+
+
+
+number = 12
+nnumber = 8
+nuumber = 64
+nummber = 2
+all = number + nnumber * nuumber / nummber
+print(all)
