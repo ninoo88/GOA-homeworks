43f99c510d37c363ec370f2f7b1a00f2aeea47a4
ninoo88 <https://github.com/ninoo88/GOA-homeworks.git>
2025-03-01 12:52:18 +0400

    commit

---
+++ Day06/Homework/hw.py
@@ -0,0 +1,21 @@
+age = input("How old are you?")
+print(age)
+
+name = input("What is your name?")
+print(name)
+
+surname = input("What is your urname?")
+print(surname)
+
+Birthday = input("when is your Birthday?")
+print(Birthday)
+
+#case-sensitive არის განსხვავება დიდ და პატარა ასიობს შიორის, ეს ნიშნავს რომ მაგალითად surname და Surname განსხვავებულად განიხლება.
+#camel case არის როდასაც სიტყვას აშორებს ან რიცხვი ან ასო, მაგალითად : my5name5is5nino
+#snake case არის როდასაც _ აშორებს ორ ან მეტ სიტყვას, მაგალითად: my_birthday_is_in_june
+
+# name = 'zura'  print(Name) აქ შეცდომა არის რომ პრინტში არის ჩაწერილი Name და უნდა იყოს name, დიდი ასოთია შაწერილი name
+# age - 13  print(age} შაქ შეცდომა არის რომ პრინტში არის ()-ის მაგივრად (} ჩაწერილი
+# Hello world = "Hello"  print(hello world) აქაც პრინტშია პატარა ასოთი დაწერილი Hello world როდესაც დიდით უნდა იყოს 
+# 2name = 'nika'  print(2name) ცვლადი არ უნდა იწყებოდეს სიცხვით დაწყებული, უნდა იწერებოდეს მაგალითად ესე: name2
+# year > 2025  print(year) აქ შეცდომა ის არის რომ > ის მაგივრად უნდა გამოიყენებოდეს =
