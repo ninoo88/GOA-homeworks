15b7faad77dc27b4ee286a318b1b9430a09786c6
ninoo88 <https://github.com/ninoo88/GOA-homeworks.git>
2025-04-16 19:04:46 +0400

    commit

---
+++ Day18/Classwork/cw.py
@@ -0,0 +1,15 @@
+for i in range(1, 11, 2):
+    print(i)
+
+# range: რიცხვების თანმიმდევრობა 
+# step: ნაბიჯი, რამდენით გადადის (აქ 2)
+# საიტერაციო ცვლადი i: თითოეული რიცხვი range-იდან
+
+for i in range(0, 100): 
+    print ((i) + "GOA Best!") 
+
+input_numbers = input("input 10 numbers : ")
+
+for i in range(10):
+    print(input_numbers [i])
+
---
+++ Day18/Homework/hw.py
@@ -0,0 +1,8 @@
+for i in range(1000):
+    print("GOA BEST")
+
+# for loop გამოიყენება მოქმედების განმეორებისთვის
+
+num = int(input("input number: "))
+for i in range(num):
+    print("GOA Best")
