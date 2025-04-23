3a8698d2d9b8c2afdf861fedf85286fb49aee0db
ninoo88 <https://github.com/ninoo88/GOA-homeworks.git>
2025-04-23 18:46:00 +0400

    commit

---
+++ Day20/Classwork/cw.py
@@ -0,0 +1,12 @@
+Number = 0
+while Number < 100:
+    print("Goa best!")
+    Number += 1
+
+num = 100
+while num >= 0:
+    print(num)
+    num = 1
+    
+
+
---
+++ Day20/Homework/hw.py
@@ -0,0 +1,23 @@
+num = 0
+while num < 3:
+    print("hello")
+    num += 1
+
+
+number = 1
+while number <= 3:
+    print(number)
+    number += 1
+
+
+response = ""
+while response != "კი":
+    response = input("კარგი? ")
+
+
+numbr = 1
+while numbr <= 5:
+    print(numbr)
+    numbr += 1
+
+# while = აკეთებს რაღაცას სანამ პირობა მართალია
