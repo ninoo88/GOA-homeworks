7e76c3f0f9bd1fa4a3dd709fb65f1b621dc3b677
ninoo88 <https://github.com/ninoo88/GOA-homeworks.git>
2025-05-17 00:13:36 +0400

    commit

---
+++ Day28/Classwork/cw.py
@@ -0,0 +1,23 @@
+
+food= ["Pizza", "Burger", "Pasta", "Sushi", "Salad"]
+print(food)
+
+list = ["xinkali", "ghomi", "qababi", 8, 88, 888]
+print(list)
+
+
+
+georgia = ["Tbilisi", "Batumi", "Kutaisi", "Zugdidi", "Telavi"]
+ages = [41, 43, 28, 13, 15]
+booleans = [True, False, True, False, True]
+my_list = georgia + ages + booleans
+
+inputttt = input("Enter a new city name: ")
+inputtt = input("Enter a new age: ")
+inputt = input("Enter a new boolean value: ")
+
+my_list [4] = inputttt
+my_list [7]= int(inputtt)  
+my_list [11]= inputt == 'true'
+
+print("Updated list:", my_list)
---
+++ Day28/homework/hw.py
@@ -0,0 +1,17 @@
+list = ["ელენე", "ნინო", "დათა", "თინა"]
+list[1] = "გურამი"
+print(list)
+
+
+list1 = ["ნინო", "დათა", "თინა", 88, 19, 12]
+list2 = [40, 48]
+list = list1 + list2
+print(list)
+
+ 
+cities = ["Tbilisi", "Batumi", "Kutaisi", "Zugdidi", "Gori"]
+ages = [46, 48, 28, 80, 60]
+booleans = [True, False, True, False, True]
+list = cities + ages + booleans
+print(list)
+
