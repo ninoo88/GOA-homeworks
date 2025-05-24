e4c3a46e3559c7dc233e29f93a1776cfa622a9a7
ninoo88 <https://github.com/ninoo88/GOA-homeworks.git>
2025-05-24 12:25:53 +0400

    commit

---
+++ Day30/Homework/hw.py
@@ -0,0 +1,11 @@
+
+#ინდექსინგი -  სიის ელემენტების წვდომა მათი პოზიციების მიხედვით.
+
+
+
+
+numbers = [1, 3, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+print(numbers[:10])
+
+fruits = ["orange", "banana", "dragonfruit", "watermelon", "strawberry"]
+print(fruits[2:5])
