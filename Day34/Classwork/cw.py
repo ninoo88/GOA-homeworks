d79859257bc9c6a4ca6127a8b7206cf794a3f43c
ninoo88 <https://github.com/ninoo88/GOA-homeworks.git>
2025-06-06 23:06:24 +0400

    commit

---
+++ Day34/Classwork/cw.py
@@ -0,0 +1,11 @@
+
+my_list = ["one", "two", "three", "four", "five"]
+
+my_list.pop(2)
+
+my_list.insert(2, "five again")
+
+my_list.append("Gurami")
+
+print("Final list:", my_list)
+print("Length of the list:", len(my_list))
