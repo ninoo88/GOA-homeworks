e7296afb4014f2a6a751de5f542363307916d90b
ninoo88 <https://github.com/ninoo88/GOA-homeworks.git>
2025-06-11 19:06:01 +0400

    commit

---
+++ Day34/Homework/hw.py
@@ -0,0 +1,22 @@
+
+def hello():
+    print("hello")
+
+for i in range(10):
+    hello()
+
+
+
+def greet(name):
+    print(f"Hello, {name}!")
+
+greet("hi")  # "hi" is the argument
+
+
+
+
+
+def greet(name):
+  
+    print(f"Hello, {name}!")
+
